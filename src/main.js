cb8c41d5dfe87cacd9d224aad04c9180f9dae0a8
Magda Skowroń <mskowron0819@gmail.com>
2017-06-12 11:16:44 +0200

    project done

--- src/main.js
+++ src/main.js
@@ -1,56 +1,122 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("form");
-    const errorText = document.querySelector(".error-message");
-    // const successText = document.querySelector(".success-message");
-    // const email = document.getElementById("email");
+    const errorText = document.getElementById('error');
+    const successText = document.getElementById('success');
     const name = document.getElementById("name");
-    // const surname = document.getElementById("surname");
-    // const pass1 = document.getElementById("pass1");
-    // const pass2 = document.getElementById("pass2");
-    const male = document.getElementById("male");
-    const female = document.getElementById("female");
-    console.log(form,errorText,name,male,female);
-    form.addEventListener("submit", function(event){
-        event.preventDefault();
-        var formValidation = true;
-        var allErrors = [];
-        errorText.innerText = "";
-        if (name.value.length <= 6){
-            allErrors.push("Masz za krótkie imie");
-            formValidation = false;
-        }
-        // if( pass1.value.length > 0 && pass1.value === pass2.value){
-        //     console.log("dobre hasła");
-        // }else {
-        //     allErrors.push("Błędne hasło");
-        //     formValidation = false;
-        // }
-        if(!(male.checked)||!(female.checked)){
-            allErrors.push("Zaznacz checkbox");
-            formValidation = false;
-        }
-        if(formValidation === true){
-            // successText.innerText = "Udało się wysłać formularz!";
-            setTimeout(function () {
-                form.submit();
-            },2000);
-        }else{
-            for( var i = 0; i < allErrors.length; i++){
-                var p = document.createElement("p");
-                p.innerText = allErrors[i];
-                errorText.appendChild(p);
-            }
-        }
-    });
+    const phone = document.getElementById("phone");
+    const gender = document.getElementsByName('gender');
+    const btn = document.querySelector('#error button');
+    // change color of gender radio input border
+    for (let i = 0; i < gender.length; i++){
+        gender[i].addEventListener('change', function(){
+            (function(radio) {
+                const inputs = document.getElementsByName(radio.name);
+                for (let i = 0; i < inputs.length; i++){
+                    let pick = inputs[i].checked ? '2px solid #1CD4A3' : '2px solid #c1c1c6';
+                    inputs[i].parentNode.style.border = pick;
+                }
+            }(this));
+        });
+    }
+    // month select options
     const month = document.getElementById('month');
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    for (let i = 0; i < months.length; i++) {
+    months.map(a=>{
         const opt = document.createElement('option');
-        opt.setAttribute("value", months[i]);
-        opt.innerText = months[i];
+        opt.setAttribute("value", a);
+        opt.innerText = a;
         month.appendChild(opt);
-        // $('#month').append($("<option></option>").attr("value", months[i]).innerTexttext(months[i]));
-    } // select default tab
-    // $('.tab[data-value="2"]').addClass('tab_active');
+    });
+    // day select options
+    const day = document.getElementById('day');
+    const days = Array.from({length: 31}, (v,k) => k+1);
+    days.map(a=>{
+        const opt = document.createElement('option');
+        opt.setAttribute("value", a);
+        opt.innerText = a;
+        day.appendChild(opt);
+    });
+    // year select options
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    const year = document.getElementById('year');
+    const years = Array.from({length: 100}, (v,k) => k+(currentYear -116));
+    years.map(a=>{
+        const opt = document.createElement('option');
+        opt.setAttribute("value", a);
+        opt.innerText = a;
+        year.appendChild(opt);
+    });
+
+    // form validation
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        let formValidation = true;
+        let allErrors = [];
+        // name validation
+        if(/\d/g.test(name.value)){
+            allErrors.push("Your name cannot contain numbers.");
+            formValidation = false;
+        }
+        if (name.value.length <= 6){
+            allErrors.push("Your name is to short.");
+            formValidation = false;
+        }
+        // phone number validation
+        if(/[a-zA-Z]+/g.test(phone.value)){
+            allErrors.push("Your phone number cannot contain letters.");
+            formValidation = false;
+        }
+        if(phone.value.length < 9){
+            allErrors.push("Your phone number must contain at least 9 characters.");
+            formValidation = false;
+        }
+        // gender validation
+        let check = false;
+        for(let i=0; i<gender.length; i++){
+            if(gender[i].checked == true){
+                check = true;
+            }
+        }
+        if(!check){
+            allErrors.push("Please select your gender.");
+            formValidation = false;
+        }
+        // date of birth validation for safari
+        if (!day.value) {
+            allErrors.push("Please select day of birth");
+            formValidation = false;
+        }
+        if (!month.value) {
+            allErrors.push("Please select month of birth");
+            formValidation = false;
+        }
+        if (!year.value) {
+            allErrors.push("Please select year of birth");
+            formValidation = false;
+        }
+
+        if(formValidation === true){
+            const div = document.querySelector("#success div");
+            const p = document.createElement("p");
+            p.innerText = "Your data was sent successfully";
+            div.appendChild(p);
+            successText.appendChild(div);
+            successText.classList.add('message');
+        }else{
+            const div = document.querySelector("#error div");
+            allErrors.map(a=>{
+                const p = document.createElement("p");
+                p.innerText = a;
+                div.appendChild(p);
+                errorText.appendChild(div);
+                errorText.classList.add('message');
+                btn.addEventListener('click', function () {
+                    errorText.classList.remove('message');
+                    p.innerText= '';
+                })
+            });
+        }
+    });
 
 });
